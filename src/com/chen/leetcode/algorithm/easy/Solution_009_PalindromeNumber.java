421431e9c1f96b38cf97ece94ab3b3d2fd0e1b85
chen <im.cchen@foxmail.com>
2019-03-04 13:36:33 +0800

    :bulb: 更新回文数题解

--- src/com/chen/leetcode/algorithm/easy/Solution_009_PalindromeNumber.java
+++ src/com/chen/leetcode/algorithm/easy/Solution_009_PalindromeNumber.java
@@ -14,39 +14,15 @@ package com.chen.leetcode.algorithm.easy;
  * @date: 2018/12/5
  **/
 public class Solution_009_PalindromeNumber {
-    public boolean isPalindrome(int x) {
-        String str = "" + x;
-        int i = 0, j = str.length() - 1;
-        while (i < j) {
-            if (str.charAt(i) == str.charAt(j)) {
-                i++;
-                j--;
-            } else {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public boolean IsPalindrome(int x) {
-        // 特殊情况：
-        // 如上所述，当 x < 0 时，x 不是回文数。
-        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
-        // 则其第一位数字也应该是 0
-        // 只有 0 满足这一属性
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
-        }
-
-        int revertedNumber = 0;
-        while (x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
+        //负数不为回文，并且个位数如果是0，那么最高位也应该是0：只有0满足条件
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int temp = 0;
+        while (x > temp) {
+            temp = temp * 10 + x % 10;
             x /= 10;
         }
-
-        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
-        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
-        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
-        return x == revertedNumber || x == revertedNumber / 10;
+        //位数可能为奇数或偶数
+        return x == temp || x == temp / 10;
     }
 }
