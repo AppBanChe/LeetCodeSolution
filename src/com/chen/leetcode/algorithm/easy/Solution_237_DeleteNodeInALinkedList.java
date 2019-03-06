dcb36bb9157eb5ed1617b367c0224877b20906a2
chen <im.cchen@foxmail.com>
2019-03-06 10:57:26 +0800

    :bulb: 提交删除链表中的节点题解

---
+++ src/com/chen/leetcode/algorithm/easy/Solution_237_DeleteNodeInALinkedList.java
@@ -0,0 +1,23 @@
+package com.chen.leetcode.algorithm.easy;
+
+/**
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ *
+ * @author: chen
+ * @date: 2019/3/6
+ **/
+public class Solution_237_DeleteNodeInALinkedList {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
