76c53df71b338fe2e8e8511d3bb60b815a49a6b3
chen <im.cchen@foxmail.com>
2019-03-06 10:29:34 +0800

    :pencil2: 提交位1的个数解答

---
+++ src/com/chen/leetcode/algorithm/easy/Solution_191_NumberOf1Bits.java
@@ -0,0 +1,21 @@
+package com.chen.leetcode.algorithm.easy;
+
+/**
+ * 编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+ * 如，输入：00000000000000000000000000001011
+ * 输出：3
+ * 解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+ *
+ * @author: chen
+ * @date: 2019/3/6
+ **/
+public class Solution_191_NumberOf1Bits {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n &= n - 1;
+            count++;
+        }
+        return count;
+    }
+}
